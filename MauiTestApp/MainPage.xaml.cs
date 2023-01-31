1abe8ab0badd97bb1175694a28a3b994cd7b65f9
Talip <talip.vu@gmail.com>
2023-01-31 10:40:04 +0100

    test commit

--- MauiTestApp/MainPage.xaml.cs
+++ MauiTestApp/MainPage.xaml.cs
@@ -17,6 +17,9 @@ namespace MauiTestApp
 
         #endregion fields
 
+        /// <summary>
+        /// Comment
+        /// </summary>
         public MainPage()
         {
             InitializeComponent();
